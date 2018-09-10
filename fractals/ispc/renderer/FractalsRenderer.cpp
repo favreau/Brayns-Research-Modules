48bbe15f7d54ce4a00157ea583f7308e75d0d6b6
Cyrille Favreau <cyrille.favreau@epfl.ch>
2018-09-11 01:06:52 +0200

    Migration to Brayns 0.7

--- fractals/ispc/renderer/FractalsRenderer.cpp
+++ fractals/ispc/renderer/FractalsRenderer.cpp
@@ -46,7 +46,7 @@ void FractalsRenderer::commit()
 
     _lightPtr = _lightArray.empty() ? nullptr : &_lightArray[0];
 
-    _bgColor = getParam3f("bgColor", ospray::vec3f(1.f));
+    _bgColor = getParam3f("bgColor", ospray::vec3f(0.f));
     _shadows = getParam1f("shadows", 0.f);
     _softShadows = getParam1f("softShadows", 0.f);
     _ambientOcclusionStrength = getParam1f("aoWeight", 0.f);
@@ -57,33 +57,22 @@ void FractalsRenderer::commit()
     _spp = getParam1i("spp", 1);
     _electronShadingEnabled = bool(getParam1i("electronShading", 0));
 
-    // Those materials are used for simulation mapping only
-    _materialData = (ospray::Data*)getParamData("materials");
-    _materialArray.clear();
-    if (_materialData)
-        for (size_t i = 0; i < _materialData->size(); ++i)
-            _materialArray.push_back(
-                ((ospray::Material**)_materialData->data)[i]->getIE());
-    _materialPtr = _materialArray.empty() ? nullptr : &_materialArray[0];
-
     // Transfer function
     _transferFunctionDiffuseData = getParamData("transferFunctionDiffuseData");
     _transferFunctionEmissionData =
         getParamData("transferFunctionEmissionData");
-    _transferFunctionSize = getParam1i("transferFunctionSize", 0);
+    _transferFunctionSize = getParam1i("transferFunctionSize", 255);
     _transferFunctionMinValue = getParam1f("transferFunctionMinValue", 0.f);
-    _transferFunctionRange = getParam1f("transferFunctionRange", 0.f);
-    _threshold = getParam1f("threshold", _transferFunctionMinValue);
+    _transferFunctionRange = getParam1f("transferFunctionRange", 255.f);
 
     // Volume
-    _volumeSamplesPerRay = getParam1i("volumeSamplesPerRay", 32);
+    _volumeSamplesPerRay = 128; // getParam1i("volumeSamplesPerRay", 32);
 
     ispc::FractalsRenderer_set(
         getIE(), (ispc::vec3f&)_bgColor, _shadows, _softShadows,
         _ambientOcclusionStrength, _ambientOcclusionDistance, _shadingEnabled,
         _randomNumber, _timestamp, _spp, _electronShadingEnabled, _lightPtr,
-        _lightArray.size(), _materialPtr, _materialArray.size(),
-        _volumeSamplesPerRay,
+        _lightArray.size(), _volumeSamplesPerRay,
         _transferFunctionDiffuseData
             ? (ispc::vec4f*)_transferFunctionDiffuseData->data
             : NULL,
@@ -91,7 +80,7 @@ void FractalsRenderer::commit()
             ? (ispc::vec3f*)_transferFunctionEmissionData->data
             : NULL,
         _transferFunctionSize, _transferFunctionMinValue,
-        _transferFunctionRange, _threshold);
+        _transferFunctionRange);
 }
 
 FractalsRenderer::FractalsRenderer()
